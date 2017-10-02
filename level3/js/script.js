ce16b26421d09bfbfc43af3b2433917a5548381e
Datta973 <mvdatta3@gmail.com>
2017-10-02 20:30:56 +0530

    Added token field

--- level3/js/script.js
+++ level3/js/script.js
@@ -1,25 +1,38 @@
-$(document).ready(function getFacebookInfo() {
+$(document).ready(function () {
 
-    var facebookToken = "EAACEdEose0cBADnZCeJnHGHfVuUjsZAZA7hRKdqU8ByRNTwemZAwwWJc9LxsDyvGnNSJQ9NkRWald11hRun7tYIrbgKRS5HGWqcwZChBHdndOZCuCTaUAehOjRueO0FqIq5zmZCtQSS544afC99s0RQUMn99rE6XLIFTfDj0eQmJJ7TXnpfs8D9E3qs2ZCZC1YkQZD";
-
-    {
-        $.ajax("https://graph.facebook.com/me?fields=id,name,birthday,education,email,gender,hometown,relationship_status,location,posts{full_picture,link,message,created_time},picture&access_token=" + facebookToken,
-            {
-
-                success: function (response) {
-                    console.log(response);
-                    setProfilePage(response);
-                    setFeedPage(response);
-                },
-                error: function (request, errorType, errorMessage) {
-                    console.log(request);
-                    console.log(errorType);
-                    alert(errorMessage);
-                },
-            });
+    if (localStorage.getItem("token")) {
+        getFacebookInfo(localStorage.getItem("token"));
     }
 
-    $("#profile").on("click", getFacebookInfo);
+    $("#submitToken").click(function () {
+        var token = $('#token').val();
+        if (token != '') {
+            localStorage.setItem("token", token);
+        }
+        if (localStorage.getItem("token")) {
+            getFacebookInfo(localStorage.getItem("token"));
+        }
+        getFacebookInfo(token);
+    });
+
+    function getFacebookInfo(accessToken) {
+        {
+            $.ajax("https://graph.facebook.com/me?fields=id,name,birthday,education,email,gender,hometown,relationship_status,location,posts{full_picture,link,message,created_time},picture&access_token=" + accessToken,
+                {
+
+                    success: function (response) {
+                        console.log(response);
+                        setProfilePage(response);
+                        setFeedPage(response);
+                    },
+                    error: function (request, errorType, errorMessage) {
+                        console.log(request);
+                        console.log(errorType);
+                        alert(request.responseJSON.error.message);
+                    },
+                });
+        }
+    }
 
     function setProfilePage(response) {
 
@@ -33,11 +46,11 @@ $(document).ready(function getFacebookInfo() {
         for (var index in response.education) {
             study += '<p> Studied at ' + response.education[index]['school'].name + '</p>';
         }
-        $('.profile-page  .card-block').append(study)
+        $('.profile-page  .card-block .user-study').html(study);
         var imgPath = '';
         imgPath = response.picture.data.url;
         console.log(imgPath);
-        $('.profile-pic').attr("src",imgPath);
+        $('.profile-pic').attr("src", imgPath);
     }
 
     function setFeedPage(response) {
@@ -45,18 +58,18 @@ $(document).ready(function getFacebookInfo() {
         var post = '';
         for (var i = 0; i < 10; i++) {
             post = '<div class="card">' +
-                '<div class="card-block">' +                
-                '<p id="name">'+ name +'</p>';
-                // '<a href="'+response.posts.data[i].link+'" class="card-link"><img src="'+response.posts.data[i].full_picture+'"></a>';               
-                if(response.posts.data[i].full_picture!=undefined) {
-                    post+= '<a href="'+response.posts.data[i].link+'" class="card-link"><img src="'+response.posts.data[i].full_picture+'"></a>';
-                 }
-                if(response.posts.data[i].message!=undefined) {
-                   post+= '<div class="card-text">'+ response.posts.data[i].message+'</div>';
-                }
-                post+=  '</div></div>'; 
-                $(".feed-page").append(post);          
+                '<div class="card-block">' +
+                '<p id="name">' + name + '</p>';
+            // '<a href="'+response.posts.data[i].link+'" class="card-link"><img src="'+response.posts.data[i].full_picture+'"></a>';               
+            if (response.posts.data[i].full_picture != undefined) {
+                post += '<a href="' + response.posts.data[i].link + '" class="card-link"><img src="' + response.posts.data[i].full_picture + '"></a>';
+            }
+            if (response.posts.data[i].message != undefined) {
+                post += '<div class="card-text">' + response.posts.data[i].message + '</div>';
+            }
+            post += '</div></div>';
+            $(".feed-page").append(post);
         }
-          
+
     }
 });
